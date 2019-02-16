083850c6f93858fa2c82e24810355a34774c42f2
Aidan Musnitzky <musnit@gmail.com>
2019-02-15 23:11:53 -0700

    ABI decoding improved

--- poke/src/assets/patchProvider.js
+++ poke/src/assets/patchProvider.js
@@ -1,11 +1,11 @@
+const abiDecoder = require('abi-decoder');
+
 const processMethod = {
   eth_call: args => {
     const contractAddress = '' + args[1][0].to.toLowerCase();
     const methodSig = args[1][0].data;
-    const contractABI = web3.reverseLookups[contractAddress];
-    const method = contractABI && contractABI[methodSig];
-    const methodName = method && method.name;
-    return `eth_call, address: ${contractAddress}, method: ${methodName}`;
+    const decodedInput = abiDecoder.decodeMethod(methodSig);
+    return `eth_call, address: ${contractAddress}, method: ${decodedInput.name}, params: ${JSON.stringify(decodedInput.params)}`;
   },
   eth_gasPrice: args => {
     return `eth_gasPrice`;
@@ -13,14 +13,13 @@ const processMethod = {
   eth_sendTransaction: args => {
     const contractAddress = '' + args[1][0].to.toLowerCase();
     const methodSig = args[1][0].data;
-    const contractABI = web3.reverseLookups[contractAddress];
-    const method = contractABI && contractABI[methodSig];
-    const methodName = method && method.name;
-    return `eth_sendTransactions, address: ${contractAddress}, method: ${methodName}`;
+    const decodedInput = abiDecoder.decodeMethod(methodSig);
+    return `eth_sendTransactions, address: ${contractAddress}, method: ${decodedInput.name}, params: ${JSON.stringify(decodedInput.params)}`;
   },
 }
 
 export const patchProvider = web3 => {
+  const contractDecoders = {};
   const currentProvider = web3.currentProvider;
   const currentProviderSend = currentProvider.send;
   const newSend = function() {
@@ -34,23 +33,12 @@ export const patchProvider = web3 => {
   currentProvider.send = newSend;
   web3.setProvider('');
   web3.setProvider(currentProvider);
-  web3.reverseLookups = {};
   const oldContract = web3.eth.Contract;
   const newContract = function() {
     console.log('Instantiating a contract');
     const contractAddress = ('' + arguments[1]).toLowerCase();
     const abi = arguments[0];
-    const reverseABIs = abi.reduce((accum, abiEntry) => {
-      accum[abiEntry.signature] = abiEntry;
-      return accum;
-    }, {});
-    const contractReversal = {};
-    contractReversal[contractAddress] = reverseABIs;
-    web3.reverseLookups = Object.assign(
-      {},
-      web3.reverseLookups,
-      contractReversal
-    );
+    abiDecoder.addABI(abi);
     return oldContract.apply(oldContract, arguments);
   }
   web3.eth.Contract = newContract;
